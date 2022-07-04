52617e0eef68fcd310fa300fdffb2dbb01670bad
Cthulhux <>
2022-07-04 17:13:58 +0000

    CDDL 1.0 -> 1.1; also, updated the Chrome manifest to v3.
    FossilOrigin-Name: 24533ff3ba52e9927e6739dea58913b5357b6ceeec4f92cae9e057d550f39f05

--- client/Chrome/scripts/background.js
+++ client/Chrome/scripts/background.js
@@ -1,3 +1,20 @@
+/**
+ * ymarks :: a self-hosted single-user bookmark sync service.
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.1 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * See the file COPYING in this distribution for details.
+ * A copy of the CDDL is also available via the Internet at
+ * https://spdx.org/licenses/CDDL-1.1.html
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the contents of the COPYING file from this
+ * distribution.
+ */
+
 _jsonObj = {};
 
 let dictOldIDsToNewIDs = { "-1": "-1" };
--- server/src/database.h
+++ server/src/database.h
@@ -1,14 +1,14 @@
 /**
- * ymarks :: a self-hosted single-user Xmarks alternative.
+ * ymarks :: a self-hosted single-user bookmark sync service.
  *
  * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
+ * Common Development and Distribution License, Version 1.1 only
  * (the "License").  You may not use this file except in compliance
  * with the License.
  *
  * See the file COPYING in this distribution for details.
  * A copy of the CDDL is also available via the Internet at
- * http://www.opensource.org/licenses/cddl1.txt
+ * https://spdx.org/licenses/CDDL-1.1.html
  *
  * When distributing Covered Code, include this CDDL HEADER in each
  * file and include the contents of the COPYING file from this
